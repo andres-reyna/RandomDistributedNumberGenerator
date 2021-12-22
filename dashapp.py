77eff9e5c14886986eb93693c550320a8510ec4f
Andres Reyna <jose.reyna@cinvestav.mx>
2021-12-22 01:59:09 -0600

    Changed presentation methods, fixed some errors

--- dashapp.py
+++ dashapp.py
@@ -27,9 +27,9 @@ from DistributionFactory import DistributionFactory
 #     suppress_callback_exceptions=True
 # )
 app = DashProxy(transforms=[
-    
+
     MultiplexerTransform(),  # makes it possible to target an output multiple times in callbacks
-    
+
 ],external_stylesheets=[dbc.themes.BOOTSTRAP],)
 
 app.config.suppress_callback_exceptions = True
@@ -45,13 +45,13 @@ distributions = [
 ]
 
 app.layout = html.Div([
-    
+
     html.Div(
 
         dbc.Container(
             [
                 html.H3("Random Distributed Number Generator", className="display-6"),
-                
+
             ],
             fluid=True,
             className="py-3 center",
@@ -78,20 +78,58 @@ app.layout = html.Div([
         #         options=[{'label': i, 'value': i} for i in available_indicators],
         #         value='Life expectancy at birth, total (years)'
         #     ),
-            
+
         # ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
     ]),
-    
+
     html.Br(),
 
     dbc.Label('Parameters:'),
 
-    dbc.Card([
-        html.Div(id='parameters-container'),
+    dbc.Card(id='parameters-container',
+             children=[
+                 html.Div(
+                     id='normal-parameters',
+                     children=[
+                         dbc.Input(id='n-mean', type='number', placeholder='Mean', step=0.1),
+                         html.Br(),
+                         dbc.Input(id='n-std', type='number', placeholder='Standard Deviation', step=0.1),
+                         html.Br()], style={'display': 'none'}),
+                 html.Div(
+                     id='binomial-parameters',
+                     children=[
+                         dbc.Input(id='b-r', type='number', placeholder='R', step=0.1),
+                         html.Br(),
+                         dbc.Input(id='b-p', type='number', placeholder='P', min=0, max=1, step=0.1),
+                         html.Br()], style={'display': 'none'}),
+                 html.Div(
+                     id='negbinomial-parameters',
+                     children=[
+                         dbc.Input(id='nb-r', type='number', placeholder='R', step=0.1),
+                         html.Br(),
+                         dbc.Input(id='nb-p', type='number', placeholder='Standard Deviation', min=0, max=1, step=0.1),
+                         html.Br()], style={'display': 'none'}),
+                 html.Div(
+                     id='exponential-parameters',
+                     children=[
+                         dbc.Input(id='e-a', type='number', placeholder='Alpha', step=0.1),
+                         html.Br(),
+                         html.Br()], style={'display': 'none'}),
 
-    ], className='col-6', style={'padding':'30px'}),
+                 html.Div(
+                     id='poisson-parameters',
+                     children=[
+                         dbc.Input(id='p-l', type='number', placeholder='lambda'),
+                         html.Br()], style={'display': 'none'})
+
+             ], className='col-6', style={'padding':'30px'}),
 
     html.Br(),
+    dbc.Input(id='sample-size', type='number', placeholder='Sample size', step=1),
+    html.Br(),
+    dbc.Button("Get Sample", id='btn-submit', color="primary", style={'width': '100%'}),
+    html.Br(),
+
     dbc.Label('Generated Sample Graph:'),
 
     dcc.Graph(id='indicator-graphic'),
@@ -103,222 +141,103 @@ app.layout = html.Div([
 ],className='container')
 
 @app.callback(
-    Output('parameters-container', 'children'),
-    Input('distribution-select', 'value')
+    Output('normal-parameters', 'style'),
+    Output('binomial-parameters', 'style'),
+    Output('negbinomial-parameters', 'style'),
+    Output('exponential-parameters', 'style'),
+    Output('poisson-parameters', 'style'),
+    Input(component_id='distribution-select', component_property='value')
 )
-def update_output(distribution):
-    content = ''
-    if distribution =='normal':
-        content = html.Div([
-
-            dbc.Input(id='n-mean', type='number', placeholder='Mean',step=0.1),
-            html.Br(),
-            dbc.Input(id='n-std', type='number', placeholder='Standard Deviation', step=0.1),
-            html.Br(),
-            dbc.Input(id='sample-size', type='number', placeholder='Sample size', step=1),
-            html.Br(),
-            dbc.Button("Get Sample", id='btn-submit', color="primary", style={'width':'100%'})
-
-        ]),
-    
-    elif distribution =='binomial':
-        content = html.Div([
-
-            dbc.Input(id='b-n', type='number', placeholder='N',step=0.1),
-            html.Br(),
-            dbc.Input(id='b-p', type='number', placeholder='P', min=0, max=1, step=0.1),
-            html.Br(),
-            dbc.Input(id='sample-size', type='number', placeholder='Sample size', step=1),
-            html.Br(),
-            dbc.Button("Get Sample", id='btn-submit', color="primary", style={'width':'100%'})
-            
-        ]),
-    
-    elif distribution =='negbinomial':
-        content = html.Div([
-
-            dbc.Input(id='nb-r', type='number', placeholder='R',step=0.1),
-            html.Br(),
-            dbc.Input(id='nb-p', type='number', placeholder='P', min=0, max=1, step=0.1),
-            html.Br(),
-            dbc.Input(id='sample-size', type='number', placeholder='Sample size', step=1),
-            html.Br(),
-            dbc.Button("Get Sample", id='btn-submit', color="primary", style={'width':'100%'})
-            
-        ]),
-
-    elif distribution == 'exponential':
-        content = html.Div([
-
-            dbc.Input(id='e-a', type='number', placeholder='alpha'),
-            html.Br(),
-            dbc.Input(id='sample-size', type='number', placeholder='Sample size', step=1),
-            html.Br(),
-            dbc.Button("Get Sample", id='btn-submit', color="primary", style={'width': '100%'})
-
-        ]),
-
-    elif distribution == 'poisson':
-        content = html.Div([
-
-            dbc.Input(id='p-l', type='number', placeholder='lambda'),
-            html.Br(),
-            dbc.Input(id='sample-size', type='number', placeholder='Sample size', step=1),
-            html.Br(),
-            dbc.Button("Get Sample", id='btn-submit', color="primary", style={'width': '100%'})
-
-        ]),
-
-    return content
+def show_hide_element(distribution):
+    if(distribution == 'normal'):
+        return [{"display": 'block'}, {"display": 'none'},
+                {"display": 'none'}, {"display": 'none'},
+                {"display": 'none'}]
+    elif (distribution == 'binomial'):
+        return [{"display": 'none'}, {"display": 'block'},
+                {"display": 'none'}, {"display": 'none'},
+                {"display": 'none'}]
+    elif (distribution == 'negbinomial'):
+        return [{"display": 'none'}, {"display": 'none'},
+                {"display": 'block'}, {"display": 'none'},
+                {"display": 'none'}]
+    elif (distribution == 'exponential'):
+        return [{"display": 'none'}, {"display": 'none'},
+                {"display": 'none'}, {"display": 'block'},
+                {"display": 'none'}]
+    elif(distribution == 'poisson'):
+        return [{"display": 'none'}, {"display": 'none'},
+                {"display": 'none'}, {"display": 'none'},
+                {"display": 'block'}]
 
 ###########################################################
 # NORMAL DISTRIBUTION
 ###########################################################
 @app.callback(
     Output('indicator-graphic', 'figure'),
-    Input('distribution-select', 'value'),
-
     Input('btn-submit', 'n_clicks'),
-
+    State('distribution-select', 'value'),
     State('n-mean', 'value'),
     State('n-std', 'value'),
-
-    # [State('b-n', 'value'),
-    # State('b-p', 'value'),],
-
-
-    Input('sample-size', 'value'),
-        prevent_initial_call=True
-    )
-def update_graph(distribution, _,sample_size, n_mean, n_std):
-    print('Params')
-    print(n_mean,' ', n_std,' ', sample_size)
-
-    fig = generate_plot(distribution, sample_size,{'mean': n_mean, 'std': n_std})
-    return fig
-
-
-@app.callback(
-    Output('pdf-graphic', 'figure'),
-    Input('distribution-select', 'value'),
-
-    Input('btn-submit', 'n_clicks'),
-
-    State('n-mean', 'value'),
-    State('n-std', 'value'),
-
-    Input('sample-size', 'value'),
-    prevent_initial_call=True
-)
-def update_graph(distribution, _, sample_size, n_mean, n_std):
-    print('Params')
-    print(n_mean, ' ', n_std, ' ', sample_size)
-
-    fig = generate_pdf(distribution, sample_size, {'mean': n_mean, 'std': n_std})
-    return fig
-
-
-###########################################################
-# BINOMIAL DISTRIBUTION
-###########################################################
-@app.callback(
-    Output('indicator-graphic', 'figure'),
-    Input('distribution-select', 'value'),
-    
-    Input('btn-submit', 'n_clicks'),
-
-    State('b-n', 'value'),
+    State('b-r', 'value'),
     State('b-p', 'value'),
-
-    Input('sample-size', 'value'),
-        prevent_initial_call=True
-    )
-def update_graph(distribution, _, sample_size,b_n, b_p):
-    print('Params')
-    print(b_n,' ', b_p,' ', sample_size)
-
-    fig = generate_plot(distribution, sample_size,{'n': b_n, 'p': b_p})
-    return fig
-
-
-@app.callback(
-    Output('pdf-graphic', 'figure'),
-    Input('distribution-select', 'value'),
-
-    Input('btn-submit', 'n_clicks'),
-
-    State('b-n', 'value'),
-    State('b-p', 'value'),
-
-    Input('sample-size', 'value'),
-    prevent_initial_call=True
-)
-def update_graph(distribution, _, sample_size, b_n, b_p):
-    print('Params')
-    print(b_n, ' ', b_p, ' ', sample_size)
-
-    fig = generate_pdf(distribution, sample_size, {'n': b_n, 'p': b_p})
-    return fig
-
-
-###########################################################
-# NEGATIVE BINOMIAL DISTRIBUTION
-###########################################################
-@app.callback(
-    Output('indicator-graphic', 'figure'),
-    Input('distribution-select', 'value'),
-    
-    Input('btn-submit', 'n_clicks'),
-
     State('nb-r', 'value'),
     State('nb-p', 'value'),
-
-    Input('sample-size', 'value'),
-        prevent_initial_call=True
-    )
-def update_graph(distribution, _, sample_size,nb_r, nb_p):
-    print('Params')
-    print(nb_r,' ', nb_p,' ', sample_size)
-
-    fig = generate_plot(distribution, sample_size,{'r': int(nb_r), 'p': float(nb_p)})
-    return fig
-
-
-###########################################################
-# POISSON DISTRIBUTION
-###########################################################
-@app.callback(
-    Output('indicator-graphic', 'figure'),
-    Input('distribution-select', 'value'),
-
-    Input('btn-submit', 'n_clicks'),
-
-    State('p-lambda', 'value'),
-
-    Input('sample-size', 'value'),
+    State('e-a', 'value'),
+    State('p-l', 'value'),
+    State('sample-size', 'value'),
     prevent_initial_call=True
 )
-def update_graph(distribution, _, sample_size, p_lambda):
-    print("Arguments: %d %d" % (sample_size, p_lambda))
-    fig = generate_plot(distribution, sample_size, {'l': p_lambda})
-    return fig
-
+def show_sample_graph(n_clics, distribution, n_mean, n_std,
+                   b_r, b_p, nb_r, nb_p, e_a, p_l, sample_size):
+    if(distribution == 'normal'):
+        fig = generate_plot(distribution, sample_size, {'mean': n_mean, 'std': n_std})
+        return fig
+    elif (distribution == 'binomial'):
+        fig = generate_plot(distribution, sample_size, {'n': b_r, 'p': b_p})
+        return fig
+    elif (distribution == 'negbinomial'):
+        fig = generate_plot(distribution, sample_size, {'r': nb_r, 'p': nb_p})
+        return fig
+    elif (distribution == 'exponential'):
+        fig = generate_plot(distribution, sample_size, {'a': e_a})
+        return fig
+    elif(distribution == 'poisson'):
+        fig = generate_plot(distribution, sample_size,{'l': p_l})
+        return fig
 
 @app.callback(
     Output('pdf-graphic', 'figure'),
-    Input('distribution-select', 'value'),
-
     Input('btn-submit', 'n_clicks'),
-
-    State('p-lambda', 'value'),
-
-    Input('sample-size', 'value'),
+    State('distribution-select', 'value'),
+    State('n-mean', 'value'),
+    State('n-std', 'value'),
+    State('b-r', 'value'),
+    State('b-p', 'value'),
+    State('nb-r', 'value'),
+    State('nb-p', 'value'),
+    State('e-a', 'value'),
+    State('p-l', 'value'),
+    State('sample-size', 'value'),
     prevent_initial_call=True
 )
-def update_graph(distribution, _, sample_size, p_lambda):
-    print("Arguments: %d %d" % (sample_size, p_lambda))
-    fig = generate_pdf(distribution, sample_size, {'l': p_lambda})
-    return fig
+def show_pdf_graph(n_clics, distribution, n_mean, n_std,
+                   b_r, b_p, nb_r, nb_p, e_a, p_l, sample_size):
+    if(distribution == 'normal'):
+        fig = generate_pdf(distribution, sample_size, {'mean': n_mean, 'std': n_std})
+        return fig
+    elif (distribution == 'binomial'):
+        fig = generate_pdf(distribution, sample_size, {'n': b_r, 'p': b_p})
+        return fig
+    elif (distribution == 'negbinomial'):
+        fig = generate_pdf(distribution, sample_size, {'r': nb_r, 'p': nb_p})
+        return fig
+    elif (distribution == 'exponential'):
+        fig = generate_pdf(distribution, sample_size, {'a': e_a})
+        return fig
+    elif(distribution == 'poisson'):
+        fig = generate_pdf(distribution, sample_size,{'l': p_l})
+        return fig
 
 
 if __name__ == '__main__':
